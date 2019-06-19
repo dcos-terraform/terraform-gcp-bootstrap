87abc33a52bceb45b6737716d88b86d3c17e824a
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-19 13:36:03 +0200

    remove dcos_version, it is not useful here

--- variables.tf
+++ variables.tf
@@ -1,87 +1,67 @@
-# Number of Instance
 variable "num_bootstrap" {
   description = "Specify the amount of bootstrap. You should have at most 1"
   default     = "1"
 }
 
-# Cluster Name
 variable "cluster_name" {
   description = "Name of the DC/OS cluster"
 }
 
-# Instance Type
 variable "machine_type" {
   description = "Instance Type"
 }
 
-# DCOS version
-variable "dcos_version" {
-  description = "Specifies which DC/OS version instruction to use. Options: 1.12.3, 1.11.10, etc. See dcos_download_path or dcos_version tree for a full list."
-}
-
-# Element by zone list
 variable "zone_list" {
   description = "Element by zone list"
   type        = "list"
   default     = []
 }
 
-# Source image to boot from
 variable "image" {
   description = "Source image to boot from"
 }
 
-# Disk Type to Leverage
 variable "disk_type" {
   description = "Disk Type to Leverage The GCE disk type. Can be either 'pd-ssd', 'local-ssd', or 'pd-standard'. (optional)"
 }
 
-# Disk Size in GB
 variable "disk_size" {
   description = "Disk Size in GB"
 }
 
-# Instance Subnetwork Name
 variable "bootstrap_subnetwork_name" {
   description = "Instance Subnetwork Name"
 }
 
-# Customer Provided Userdata
 variable "user_data" {
   description = "User data to be used on these instances (cloud-init)"
   default     = ""
 }
 
-# SSH User
 variable "ssh_user" {
   description = "SSH User"
 }
 
-# SSH Public Key
 variable "public_ssh_key" {
   description = "SSH Public Key"
 }
 
-# Add special tags to the resources created by this module
 variable "tags" {
   description = "Add custom tags to all resources"
   type        = "list"
   default     = []
 }
 
-# Deploy instance with preemptible scheduling. (bool)
 variable "scheduling_preemptible" {
   description = "Deploy instance with preemptible scheduling. (bool)"
   default     = "false"
 }
 
-# Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
   description = "Format the hostname inputs are index+1, region, cluster_name"
   default     = "%[3]s-bootstrap%[1]d-%[2]s"
 }
 
-# The operating system to use. Instead of using your own AMI you could use a provided OS.
 variable "dcos_instance_os" {
   description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
   default     = "centos_7.4"
